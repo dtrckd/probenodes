62bcc62a8ce6a9f22aad7b562a061869cf15bd4a
dtrckd <ddtracked@gmail.com>
2017-02-24 13:05:23 +0100

    socat

--- statserver.sh
+++ statserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PORT="8000"
 ./probenodes.py  &
 
-while true ; do echo -e "$(probenodes.py html)" | nc -l -p 8000  ; done 
+#while true ; do echo -e "$(probenodes.py html)" | nc -l -p $PORT; done 
+socat TCP4-LISTEN:$PORT,fork EXEC:"./probenodes.py html"
