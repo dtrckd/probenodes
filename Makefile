f305fce740f0af040c5244dacc58d537f0ccd4ee
dtrckd <ddtracked@gmail.com>
2017-02-24 14:42:57 +0100

    Makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+install:
+	ln -s $(PWD)/amaburn1.sh $(HOME)/bin/
+	touch pb_mem pb_cpu
--- statserver.sh
+++ statserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PORT="8000"
-./probenodes.py  &
+./probenodes.py &
 
 #while true ; do echo -e "$(probenodes.py html)" | nc -l -p $PORT; done 
 socat TCP4-LISTEN:$PORT,fork EXEC:"./probenodes.py html"
