7a0de85b7cc99a40a56cff789787915cc6bddefc
adulac <adulac@zombie-dust.imag.fr>
2018-01-03 11:34:24 +0100

    print pid in kpnodes

--- kpnode.sh
+++ kpnode.sh
@@ -4,4 +4,6 @@
 pid1=$(ps -u | grep "./probenodes.py$" | tr -s ' ' | cut  -d ' ' -f 2 )
 pid2=$(ps -u | grep "socat.*probenodes" | tr -s ' ' | cut  -d ' ' -f 2 )
 
-kill -9  $pid1 $pid2
+echo "pid ot kill: $pid1 $pid12"
+
+kill -9  "$pid1" "$pid2"
